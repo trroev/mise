cc09da30bb73be793b09e952f36588d23a00dbb6
Trevor Mathiak <trevor.mathiak@fusionary.com>
2026-04-30 14:54:06 -0400

    Revert "fix(ui): teach tailwind-merge our custom font-size tokens to prevent text color collapse"
    This reverts commit ff4e10da8cb2926569741b6f07753d89d679e281.

--- packages/ui/src/utils/cn/index.ts
+++ packages/ui/src/utils/cn/index.ts
@@ -1,25 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
-import { extendTailwindMerge } from "tailwind-merge"
-
-const FONT_SIZES = [
-  "display",
-  "heading-xl",
-  "heading-lg",
-  "heading-md",
-  "body-lg",
-  "body",
-  "body-sm",
-  "caption",
-  "label",
-] as const
-
-const twMerge = extendTailwindMerge({
-  extend: {
-    classGroups: {
-      "font-size": [{ text: [...FONT_SIZES] }],
-    },
-  },
-})
+import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: Array<ClassValue>): string =>
   twMerge(clsx(inputs))
